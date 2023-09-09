13f6177f69996f6875962f637f17ed19ba2ad90e
Siyu Liu <siyu.java.web@gmail.com>
2023-09-09 16:53:41 +1000

    adds render,save and delete note functions

--- scripts/bottom-app-bar.js
+++ scripts/bottom-app-bar.js
@@ -1,4 +1,5 @@
 const bottomMenu = document.querySelector('.bottom-menu');
+const notesApp = document.querySelector(".app-notes");
 
 export function showBottomAppBar() { 
     bottomMenu.classList.add('ease-in');
@@ -7,3 +8,4 @@ export function showBottomAppBar() {
 export function hideBottomAppBar() { 
     bottomMenu.classList.remove('ease-in');
 }
+
